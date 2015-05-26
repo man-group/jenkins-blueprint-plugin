9a043bf2cf4ee99e7101b4588cd1f9c6e1e6f489
Wilfred Hughes <whughes@ahl.com>
2015-05-26 18:27:49 +0100

    We don't need any global settings.

--- src/main/java/org/jenkinsci/plugins/yamlproject/HelloWorldBuilder.java
+++ src/main/java/org/jenkinsci/plugins/yamlproject/HelloWorldBuilder.java
@@ -30,7 +30,6 @@ import java.io.IOException;
  * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
  * method will be invoked. 
  *
- * @author Kohsuke Kawaguchi
  */
 public class HelloWorldBuilder extends Builder {
 
@@ -53,12 +52,7 @@ public class HelloWorldBuilder extends Builder {
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
         // This is where you 'build' the project.
         // Since this is a dummy, we just say 'hello world' and call that a build.
-
-        // This also shows how you can consult the global configuration of the builder
-        if (getDescriptor().getUseFrench())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
+        listener.getLogger().println("Hello, "+name+"!");
         return true;
     }
 
@@ -80,15 +74,6 @@ public class HelloWorldBuilder extends Builder {
      */
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
-        /**
-         * To persist global configuration information,
-         * simply store it in a field and call save().
-         *
-         * <p>
-         * If you don't want fields to be persisted, use <tt>transient</tt>.
-         */
-        private boolean useFrench;
-
         /**
          * In order to load the persisted global configuration, you have to 
          * call load() in the constructor.
@@ -128,27 +113,6 @@ public class HelloWorldBuilder extends Builder {
          */
         public String getDisplayName() {
             return "Say hello world";
-        }
-
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            // To persist global configuration information,
-            // set that to properties and call save().
-            useFrench = formData.getBoolean("useFrench");
-            // ^Can also use req.bindJSON(this, formData);
-            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
-            save();
-            return super.configure(req,formData);
-        }
-
-        /**
-         * This method returns true if the global configuration says we should speak French.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
-         */
-        public boolean getUseFrench() {
-            return useFrench;
         }
     }
 }
